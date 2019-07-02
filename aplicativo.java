4ba6313496354972a23d0f58862354935900060a
Bryan <you@example.com>
2019-07-01 22:11:29 -0500

    version

---
+++ aplicativo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class aplicativo{
+ public static void main(String [] args){
+
+	float num,dem;
+
+	Scanner lee = new Scanner(System.in);
+
+	System.out.println("ingrese el numerador");
+	num=lee.nextFloat();
+
+	System.out.println("ingrese el denominador");
+	dem=lee.nextFloat();
+}
+}
